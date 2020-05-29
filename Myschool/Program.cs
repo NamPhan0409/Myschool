368ee92ae2322e5123a356d67aade21d71ccbc17
namphan <thienbao3428@gmail.com>
2020-05-29 08:45:53 +0700

    testgit

---
+++ Myschool/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Myschool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st = new Student("1","123");
+            Console.WriteLine("{0},{1}", st.ID, st.FullName);
+        }
+    }
+}
---
+++ Myschool/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myschool
+{
+    class Student
+    {
+        protected string _id;
+        protected string _fullname;
+        public string ID { get { return _id ; } set { ID = value  ; } }
+        public string FullName { get {return _fullname ; } set { FullName=value ; } }
+
+        public Student(string ID, String FullName)
+        {
+            _id = ID;
+            _fullname = FullName;
+        }
+    }
+}
